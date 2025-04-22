d0132e53836f8ae2971c8664a9063c30525fefaf
ElisDN <mail@elisdn.ru>
2025-04-22 13:45:32 +0300

    HTTP client

--- app.php
+++ app.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace demo;
 
 use Closure;
+use Exception;
+use RuntimeException;
 
 final class Loop
 {
@@ -68,16 +70,62 @@ function interval(int $timeout, Closure $callback): void
     timeout($timeout, $task);
 }
 
+function fetch(string $url, Closure $onSuccess, ?Closure $onError = null): void
+{
+    Loop::enqueue(function () use ($url, $onSuccess, $onError) {
+        $segments = parse_url($url);
+
+        $stream = fsockopen($segments['host'], $segments['port'] ?? 80, $errorCode, $errorMessage, 3);
+
+        if (!$stream) {
+            $onError(new RuntimeException("$errorMessage ($errorCode)"));
+        } else {
+            stream_set_blocking($stream, false);
+
+            $uri = ($segments['path'] ?? '/') . (!empty($segments['query']) ? '?' . $segments['query'] : '');
+            $request = "GET $uri HTTP/1.1\r\n";
+            $request .= "Host: {$segments['host']}\r\n";
+            $request .= "Connection: Close\r\n\r\n";
+
+            fwrite($stream, $request);
+
+            $buffer = '';
+
+            $task = function () use ($stream, &$buffer, $onSuccess, &$task) {
+                if (!feof($stream)) {
+                    $buffer .= fgets($stream, 128);
+                    Loop::enqueue($task);
+                } else {
+                    fclose($stream);
+                    [, $body] = explode("\r\n\r\n", $buffer);
+                    $onSuccess($body);
+                }
+            };
+
+            Loop::enqueue($task);
+        }
+    });
+}
+
 Loop::enqueue(function () {
     echo 'Begin' . PHP_EOL;
 
-    timeout(5, function () {
+    timeout(2, function () {
         echo 'Hello' . PHP_EOL;
     });
 
-    interval(3, function () {
-        echo 'World' . PHP_EOL;
+    interval(1, function () {
+        echo date('Y-m-d H:i:s') . PHP_EOL;
     });
+
+    fetch('http://weather',
+        function (string $body) {
+            echo 'Weather: Given ' . $body . PHP_EOL;
+        },
+        function (Exception $error) {
+            echo 'Weather: Error ' . $error->getMessage() . PHP_EOL;
+        }
+    );
 
     interval(1, function () {
         echo date('Y-m-d H:i:s') . PHP_EOL;
