8510628fb74fe6296c93cfc236ad00aa2a96750a
ElisDN <mail@elisdn.ru>
2025-04-22 11:56:59 +0300

    Code fragments

--- app.php
+++ app.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 
 namespace demo;
 
-echo 1 . PHP_EOL;
-echo 2 . PHP_EOL;
-echo 3 . PHP_EOL;
-echo 4 . PHP_EOL;
+$code1 = function () {
+    echo 1 . PHP_EOL;
+};
+
+$code2 = function () {
+    echo 2 . PHP_EOL;
+};
+
+$code3 = function () {
+    echo 3 . PHP_EOL;
+};
+
+$code4 = function () {
+    echo 4 . PHP_EOL;
+};
+
+$code1();
+$code2();
+$code3();
+$code4();
