8aa1f66c2aac6af5fa0a1051280da719296c322a
ElisDN <mail@elisdn.ru>
2025-04-23 16:40:40 +0300

    Sync example

--- app.php
+++ app.php
@@ -8,7 +8,22 @@ echo 'Begin' . PHP_EOL;
 
 echo 1 . PHP_EOL;
 echo 2 . PHP_EOL;
+
+echo "Weather: Fetch 1" . PHP_EOL;
+$body1 = file_get_contents('http://weather/?day=1');
+echo "Weather: Given 1 " . $body1 . PHP_EOL;
+
+echo "Weather: Fetch 2" . PHP_EOL;
+$body2 = file_get_contents('http://weather/?day=2');
+echo "Weather: Given 2 " . $body2 . PHP_EOL;
+
 echo 3 . PHP_EOL;
+
+for ($i = 1; $i < 5; $i++) {
+    echo date('Y-m-d H:i:s') . PHP_EOL;
+    sleep(1);
+}
+
 echo 4 . PHP_EOL;
 
 echo 'End' . PHP_EOL;
